cd0d39017daeefdfd141f0e325e22255bca8dd8c
erickango <eric.ngo1xyz@gmail.com>
2015-10-21 08:23:20 -0700

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -1,4 +1,14 @@
-class Farm 
-{     
-   //your code here
-}
+class Farm  {     
+   private Animal[] aBunchOfAnimals = new Animal[4];    
+   public Farm()     {       
+      aBunchOfAnimals[0] = new Cow("cow","moo");           
+      aBunchOfAnimals[1] = new Chick("chick","cluck");       
+      aBunchOfAnimals[2] = new Pig("pig","oink"); 
+      aBunchOfAnimals[3] = new Chick();
+         }         
+   public void animalSounds()    {       
+     for (int nI=0; nI < aBunchOfAnimals.length-1; nI++)       {          
+       System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());       
+      }    
+    } 
+} 
