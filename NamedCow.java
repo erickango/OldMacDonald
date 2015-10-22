41b56f5ccc41008af6e01a4f6ed152134b7f7bd7
erickango <eric.ngo1xyz@gmail.com>
2015-10-22 08:17:26 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,20 @@
-class NamedCow
+class NamedCow 
 {
+	private String cowName, myType, mySound;
 
+	public NamedCow(String type, String cname, String sound){
+		myType = type;
+		mySound = sound;
+		cowName = cname;
+	}          
+    public NamedCow()     {         
+        myType = "cow";         
+        mySound = "moo";
+        cowName = "I dont know";     
+    }      
+    public String getSound(){return mySound;}     
+    public String getType(){return myType;} 
+	public String getName(){
+		return cowName;
+	}
 }
